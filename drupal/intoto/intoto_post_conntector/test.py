c6f80cfc9b5cffea368b3f21eea541bcad280fdb
Roi <roikedem@gmail.com>
2015-10-20 17:41:42 +0000

    changed reaction on post: both IP and customer have notes filled according to sent form

---
+++ drupal/intoto/intoto_post_conntector/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import requests
+import json
+import sys
+
+_headers = {'content-type': 'application/json'}
+url = 'http://0015-ormil.dev.into.to/react/contact_us_form'
+
+#_data = {"name": "Roi Kedem", "phone": "035587745", "email" : "roiroi@roiroi.com", "message":"here is my message blablabla"}
+_data = {"name": sys.argv[1], "phone": "035587745", "email" : "roiroi@roiroi.com", "message":"here is my message blablabla for " + sys.argv[1]}
+
+#r = requests.post(url, params=params, data=json.dumps(data), headers=headers)
+r = requests.post(url, data = _data)
+#r = requests.post(url, data={'@number': 12524, '@type': 'issue', '@action': 'show'})
+print r.text
